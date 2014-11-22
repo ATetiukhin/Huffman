f30e1ddb3418f35229a96edce9188169fe219649
ATetiukhin <artemtetiukhin@yandex.ru>
2014-11-23 02:20:10 +0300

    Unit test - gtest

---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+
+#include "gtest/gtest.h"
+//#pragma comment(lib, "gtest")
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
